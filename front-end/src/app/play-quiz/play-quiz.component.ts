e2e4e076ff5435b45535df95b17e4a0d8dd111d0
Louis Raffin <louis.raffin@etu.univ-cotedazur.fr>
2020-03-24 00:42:41 +0100

    - implement play quiz (get quiz & questions on play page) #7

--- front-end/src/app/play-quiz/play-quiz.component.ts
+++ front-end/src/app/play-quiz/play-quiz.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Quiz} from '../../models/quiz.model';
 import {ActivatedRoute} from '@angular/router';
 import {QuizService} from '../../services/quiz.service';
+import {Question} from '../../models/question.model';
 
 @Component({
   selector: 'app-play-quiz',
@@ -11,16 +12,26 @@ import {QuizService} from '../../services/quiz.service';
 export class PlayQuizComponent implements OnInit {
 
   public quiz: Quiz;
+  public questionNumber: number;
+  public question: Question;
 
   constructor(private route: ActivatedRoute, private quizService: QuizService) {
     this.quizService.quizSelected$.subscribe((quiz) => this.quiz = quiz); // set class var quiz
+
+    this.questionNumber = 0;
+
+    if (this.questionNumber <= this.quiz.questions.length) {
+      this.question = this.quiz.questions[this.questionNumber];
+    } else {
+      console.log('redirect to result page');
+    }
   }
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
     this.quizService.setSelectedQuiz(id);
 
-    console.log('test', this.quiz);
+    // this.questionNumber = +this.route.snapshot.paramMap.get('questionNumber');  // +in front of string cast string to int
   }
 
 }
