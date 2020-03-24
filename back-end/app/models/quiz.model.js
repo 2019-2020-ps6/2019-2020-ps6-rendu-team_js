7e196b242989078b277d08f80cb9cb03ba2e9eca
Laradefendini <lara.defendini@etu.unice.fr>
2020-03-24 09:21:29 +0100

    #4 layout changes for the display of the quizzes and #8 added "difficulty" attribute for the quiz

--- back-end/app/models/quiz.model.js
+++ back-end/app/models/quiz.model.js
@@ -4,4 +4,5 @@ const BaseModel = require('../utils/base-model.js')
 module.exports = new BaseModel('Quiz', {
   theme: Joi.string().required(),
   name: Joi.string().required(),
+  difficulty: Joi.string().required(),
 })
