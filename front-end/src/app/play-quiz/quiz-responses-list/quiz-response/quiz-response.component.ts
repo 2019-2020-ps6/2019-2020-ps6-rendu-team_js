e779ac7aac50756a2e7bdfd689adba6533044e75
Louis Raffin <louis.raffin@etu.univ-cotedazur.fr>
2020-04-21 11:52:08 +0200

    - adapt play quiz to font size #20

--- front-end/src/app/play-quiz/quiz-responses-list/quiz-response/quiz-response.component.ts
+++ front-end/src/app/play-quiz/quiz-responses-list/quiz-response/quiz-response.component.ts
@@ -5,6 +5,7 @@ import {Router} from '@angular/router';
 import {QuizService} from '../../../../services/quiz.service';
 import {Settings} from '../../../../models/settings.model';
 import {SettingsService} from '../../../../services/settings.service';
+import {forEachComment} from "tslint";
 
 @Component({
   selector: 'app-quiz-response',
@@ -21,21 +22,33 @@ export class QuizResponseComponent implements OnInit {
   constructor(private router: Router, public quizService: QuizService, private settingsService: SettingsService) {
     this.settings = settingsService.settings;
     console.log('user settings', this.settings);
-    this.adaptCssToSettings();
   }
 
   ngOnInit() {
+    this.adaptCssToSettings();
   }
 
   adaptCssToSettings() {
     if (this.settings != null) {
-      document.documentElement.style.setProperty('--fontSize', this.settings.fontSize + '');
-      document.documentElement.style.setProperty('--contraste', this.settings.contraste + '');
-      // console.log('css property', document.documentElement.style.getPropertyValue('--contraste'));
-      document.documentElement.style.setProperty('--fontSize', this.settings.fontSize + '');
-      document.documentElement.style.setProperty('--moteur', this.settings.handicapMoteur + '');
-      document.documentElement.style.setProperty('--visuel', this.settings.handicapVisuel + '');
-      document.documentElement.style.setProperty('--tailleSelec', this.settings.tailleSelection + '');
+            const buttons = document.getElementsByClassName('responseButton');
+      // tslint:disable-next-line:prefer-for-of
+      for (let i = 0; i < buttons.length; i++) {
+        if (this.settings.fontSize === 1) {
+          // @ts-ignore
+          buttons[i].style.setProperty('--fontSize', '500%');
+        } else {
+          // @ts-ignore
+          buttons[i].style.setProperty('--fontSize', '1000%');
+        }
+
+        // TODO add condition and css vars for :
+        // document.documentElement.style.setProperty('--contraste', this.settings.contraste + '');
+        // // console.log('css property', document.documentElement.style.getPropertyValue('--contraste'));
+        // document.documentElement.style.setProperty('--fontSize', this.settings.fontSize + '');
+        // document.documentElement.style.setProperty('--moteur', this.settings.handicapMoteur + '');
+        // document.documentElement.style.setProperty('--visuel', this.settings.handicapVisuel + '');
+        // document.documentElement.style.setProperty('--tailleSelec', this.settings.tailleSelection + '');
+      }
     }
   }
 
