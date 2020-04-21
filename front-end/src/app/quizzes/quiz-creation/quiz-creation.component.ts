fdf47513f38e3b54a1f86b1193616d6350fb8a58
Laradefendini <lara.defendini@etu.unice.fr>
2020-04-21 07:00:18 +0200

    #8 added button to create a new theme

--- front-end/src/app/quizzes/quiz-creation/quiz-creation.component.ts
+++ front-end/src/app/quizzes/quiz-creation/quiz-creation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 import { QuizService } from '../../../services/quiz.service';
@@ -13,15 +13,6 @@ import {BehaviorSubject} from 'rxjs';
   styleUrls: ['./quiz-creation.component.scss']
 })
 export class QuizCreationComponent implements OnInit {
-
-  // Note: We are using here ReactiveForms to create our form. Be careful when you look for some documentation to
-  // avoid TemplateDrivenForm (another type of form)
-
-  /**
-   * QuizForm: Object which manages the form in our component.
-   * More information about Reactive Forms: https://angular.io/guide/reactive-forms#step-1-creating-a-formgroup-instance
-   */
-  public quizForm: FormGroup;
   // private themeList: Theme[];
   // private themeList$: BehaviorSubject<Quiz[]> = new BehaviorSubject(this.themeList);
 
@@ -41,7 +32,23 @@ export class QuizCreationComponent implements OnInit {
     // Advanced validation: https://angular.io/guide/form-validation#reactive-form-validation
   }
 
+  // Note: We are using here ReactiveForms to create our form. Be careful when you look for some documentation to
+  // avoid TemplateDrivenForm (another type of form)
+
+  /**
+   * QuizForm: Object which manages the form in our component.
+   * More information about Reactive Forms: https://angular.io/guide/reactive-forms#step-1-creating-a-formgroup-instance
+   */
+  public quizForm: FormGroup;
+
+  @Output()
+  isBackPressed: EventEmitter<boolean> = new EventEmitter<boolean>();
+
   ngOnInit() {
+  }
+
+  backPressed() {
+    this.isBackPressed.emit(true);
   }
 
   addQuiz() {
