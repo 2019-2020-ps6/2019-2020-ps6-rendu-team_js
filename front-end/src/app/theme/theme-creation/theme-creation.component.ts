bc9ef5d6bf584f63b05bdf068888dcfa4d9f97f7
Laradefendini <lara.defendini@etu.unice.fr>
2020-04-21 18:40:12 +0200

    #33 theme creation page (but button "créer" not working yet)

--- front-end/src/app/theme/theme-creation/theme-creation.component.ts
+++ front-end/src/app/theme/theme-creation/theme-creation.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {QuizService} from '../../../services/quiz.service';
+import {ThemesService} from '../../../services/themes.service';
+import {Theme} from '../../../models/theme.model';
 
 @Component({
   selector: 'app-theme-creation',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThemeCreationComponent implements OnInit {
 
-  constructor() { }
+  public themeForm: FormGroup;
+
+  @Output()
+  isBackPressed: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  constructor(public formBuilder: FormBuilder, public quizService: QuizService, public themesService: ThemesService) {
+    this.themeForm = this.formBuilder.group({
+      name: [''],
+      color: ['']
+    });
+  }
 
   ngOnInit() {
+  }
+
+  addTheme() {
+    // We retrieve here the theme object from the themeForm and we cast the type "as Theme".
+    const themeToCreate: Theme = this.themeForm.getRawValue() as Theme;
+
+    this.themesService.addTheme(themeToCreate);
+  }
+
+  backPressed() {
+    this.isBackPressed.emit(true);
   }
 
 }
