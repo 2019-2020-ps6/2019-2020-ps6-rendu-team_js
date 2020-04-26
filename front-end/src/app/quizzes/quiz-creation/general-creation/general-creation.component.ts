05a11b52b3539a4d810a95823c981b9525ba5049
GuillaumeSavornin <guillaume.savornin@etu.unice.fr>
2020-04-27 01:46:56 +0200

    #31 Fixed an issue in the create quiz general that didn't show the current theme selected (only fixed in the create not in the edit)

--- front-end/src/app/quizzes/quiz-creation/general-creation/general-creation.component.ts
+++ front-end/src/app/quizzes/quiz-creation/general-creation/general-creation.component.ts
@@ -6,105 +6,107 @@ import {ThemesService} from '../../../../services/themes.service';
 import {Quiz} from '../../../../models/quiz.model';
 
 @Component({
-  selector: 'app-general-creation',
-  templateUrl: './general-creation.component.html',
-  styleUrls: ['./general-creation.component.scss']
+    selector: 'app-general-creation',
+    templateUrl: './general-creation.component.html',
+    styleUrls: ['./general-creation.component.scss']
 })
 export class GeneralCreationComponent implements OnInit {
 
-  themeList: Theme[];
-  themeSelected: Theme;
-  name = '';
-  difficulty = '';
-  isCreateThemeOpen: boolean;
-  isLoading: boolean;
-  quizForm: FormGroup;
+    themeList: Theme[];
+    themeSelected: Theme;
+    name = '';
+    difficulty = '';
+    isCreateThemeOpen: boolean;
+    isLoading: boolean;
+    quizForm: FormGroup;
 
-  @Output()
-  isBackPressed: EventEmitter<boolean> = new EventEmitter<boolean>();
+    @Output()
+    isBackPressed: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-  @Output()
-  isCreateThemeOpened: EventEmitter<boolean> = new EventEmitter<boolean>();
+    @Output()
+    isCreateThemeOpened: EventEmitter<boolean> = new EventEmitter<boolean>();
 
 
-  @Input()
-  quiz: Quiz;
+    @Input()
+    quiz: Quiz;
 
-  @Output()
-  quizEmitter: EventEmitter<Quiz> = new EventEmitter<Quiz>();
+    @Output()
+    quizEmitter: EventEmitter<Quiz> = new EventEmitter<Quiz>();
 
-  constructor(private formBuilder: FormBuilder,
-              private  quizService: QuizService,
-              private themesService: ThemesService) {
+    constructor(private formBuilder: FormBuilder,
+                private  quizService: QuizService,
+                private themesService: ThemesService) {
 
-    this.themesService.themes$.subscribe((t) => {
-      this.themeList = t;
-    });
+        this.themesService.themes$.subscribe((t) => {
+            this.themeList = t;
+        });
 
-    this.themesService.setThemes();
+        if (this.themesService.themes === undefined || this.themesService.themes === null) {
+            this.themesService.setThemes();
+        }
 
-  }
-
-  ngOnInit() {
-    this.isLoading = false;
-    this.isCreateThemeOpen = false;
-
-    if (this.quiz) {
-      this.name = this.quiz.name;
-      this.difficulty = this.quiz.difficulty;
-      this.themeSelected = this.quiz.theme;
     }
 
+    ngOnInit() {
+        this.isLoading = false;
+        this.isCreateThemeOpen = false;
 
-    this.quizForm = this.formBuilder.group({
-      name: [this.name, [Validators.required]],
-      theme: [this.themeSelected, [Validators.required]],
-      difficulty: [this.difficulty, [Validators.required]]
-    });
-  }
+        if (this.quiz) {
+            this.name = this.quiz.name;
+            this.difficulty = this.quiz.difficulty;
+            this.themeSelected = this.quiz.theme;
+        }
 
-  isWindowOpen() {
-    return this.isCreateThemeOpen;
-  }
 
-  resetValues() {
-    this.quizForm.reset();
-    this.quizEmitter.emit({} as Quiz);
-  }
-
-  updatePartOfQuiz() {
-    const quizName = this.quizForm.get('name').value;
-    const theme = this.quizForm.get('theme').value;
-    const diff = this.quizForm.get('difficulty').value;
-
-    const quiz = {
-      name: quizName,
-      themeId: theme.id,
-      theme: theme as Theme,
-      difficulty: diff,
-    } as Quiz;
-
-    if (this.quiz === undefined) {
-      this.quiz = quiz;
+        this.quizForm = this.formBuilder.group({
+            name: [this.name, [Validators.required]],
+            theme: [this.themeSelected, [Validators.required]],
+            difficulty: [this.difficulty, [Validators.required]]
+        });
     }
 
-    this.quiz.name = quizName;
-    this.quiz.themeId = theme.id;
-    this.quiz.theme = theme;
-    this.quiz.difficulty = diff;
+    isWindowOpen() {
+        return this.isCreateThemeOpen;
+    }
+
+    resetValues() {
+        this.quizForm.reset();
+        this.quizEmitter.emit({} as Quiz);
+    }
+
+    updatePartOfQuiz() {
+        const quizName = this.quizForm.get('name').value;
+        const theme = this.quizForm.get('theme').value;
+        const diff = this.quizForm.get('difficulty').value;
+
+        const quiz = {
+            name: quizName,
+            themeId: theme.id,
+            theme: theme as Theme,
+            difficulty: diff,
+        } as Quiz;
+
+        if (this.quiz === undefined) {
+            this.quiz = quiz;
+        }
+
+        this.quiz.name = quizName;
+        this.quiz.themeId = theme.id;
+        this.quiz.theme = theme;
+        this.quiz.difficulty = diff;
 
 
-    console.log(this.quiz);
-    this.quizEmitter.emit(quiz);
-  }
+        console.log(this.quiz);
+        this.quizEmitter.emit(quiz);
+    }
 
-  openWindowsCreateTheme(b: boolean) {
-    this.isCreateThemeOpen = !this.isCreateThemeOpen;
-    this.isCreateThemeOpened.emit(b);
-  }
+    openWindowsCreateTheme(b: boolean) {
+        this.isCreateThemeOpen = !this.isCreateThemeOpen;
+        this.isCreateThemeOpened.emit(b);
+    }
 
-  setSelectedTheme(theme: Theme) {
-    this.themeSelected = theme;
-    console.log(this.themeSelected);
-  }
+    setSelectedTheme(theme: Theme) {
+        this.themeSelected = theme;
+        console.log(this.themeSelected);
+    }
 }
